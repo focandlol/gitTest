88ab9f482116b4e73cc351f7a0e76b1599f90748
focandlol <focandlol@naver.com>
2024-10-31 11:31:14 +0900

    gitTest2 commit

--- gitTest/src/Hello.java
+++ gitTest/src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world22");
+
+        System.out.println("other hello world");
     }
 }
